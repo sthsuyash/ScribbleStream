6a0d3557ee58e09f4a1fa918d30acdc45f42bc53
Suyash Shrestha <sthasuyash11@gmail.com>
2022-08-03 14:22:23 +0545

    initial commit

---
+++ src/main/java/com/blog/blogsite/BlogSiteApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/blogsite/BlogSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
